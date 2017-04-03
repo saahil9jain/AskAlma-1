7d780ab158a4f4b1838991738aa3ace46699fff7
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-77.dyn.columbia.edu>
2017-04-03 15:46:10 -0400

    moved static back

--- webapp/askalma/urls.py
+++ webapp/askalma/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 app_name = 'askalma'
 urlpatterns = [
 url(r'^$', views.index, name='index'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
