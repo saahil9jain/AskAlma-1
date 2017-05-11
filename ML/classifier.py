8e9f94215459050bd12530b2d648a8e53a95b13a
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-117.dyn.columbia.edu>
2017-05-10 23:46:25 -0400

    categorization templating

--- ML/classifier.py
+++ ML/classifier.py
@@ -4,28 +4,28 @@ from watson_developer_cloud import NaturalLanguageClassifierV1
 nlc = NaturalLanguageClassifierV1(username = "92475337-a51a-47f2-b42e-69e772b1e3cc", password = "NdHjAGCYDeOf")
 
 classifiers = nlc.list()
-print(json.dumps(classidiers, indent=2))
+print(json.dumps(classifiers, indent=2))
 
 
 # create a classifier
 # with open('../resources/weather_data_train.csv', 'rb') as training_data:
-#     print(json.dumps(natural_language_classifier.create(
+#     print(json.dumps(nlc.create(
 # training_data=training_data, name='weather'), indent=2))
 
 # replace 2374f9x68-nlc-2697 with your classifier id
-status = natural_language_classifier.status('2374f9x68-nlc-2697')
+status = nlc.status('2374f9x68-nlc-2697')
 print(json.dumps(status, indent=2))
 
 if status['status'] == 'Available':
-    classes = natural_language_classifier.classify('2374f9x68-nlc-2697',
+    classes = nlc.classify('2374f9x68-nlc-2697',
                                                    'How hot will it be '
                                                    'tomorrow?')
     print(json.dumps(classes, indent=2))
 
-# delete = natural_language_classifier.remove('2374f9x68-nlc-2697')
+# delete = nlc.remove('2374f9x68-nlc-2697')
 # print(json.dumps(delete, indent=2))
 
 # example of raising a WatsonException
 # print(json.dumps(
-#     natural_language_classifier.create(training_data='', name='weather3'),
+#     nlc.create(training_data='', name='weather3'),
 #     indent=2))
--- ML/ibmWatson.py
+++ ML/ibmWatson.py
@@ -5,5 +5,5 @@ nlu = watson_developer_cloud.NaturalLanguageUnderstandingV1(version='2017-05-11'
                                                             username='92475337-a51a-47f2-b42e-69e772b1e3cc',
                                                             password='NdHjAGCYDeOf')
 output = nlu.analyze(text='this is my experimental text.  Bruce Banner is the Hulk and Bruce Wayne is BATMAN! Superman fears not Banner, but Wayne.',
-            features=[features.Sentiment()])
+            features=[features.Sentiment()]) #select features.Categories() to get categories
 print(output)
